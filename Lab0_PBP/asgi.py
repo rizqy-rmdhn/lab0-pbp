08f2300ac61ee974aef8d57dfc75287a40b3a8ef
Muhammad Rizqy Ramadhan <rizqy.r@gmail.com>
2022-09-01 13:50:12 +0700

    Init repo

---
+++ Lab0_PBP/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Lab0_PBP project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lab0_PBP.settings')
+
+application = get_asgi_application()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lab0pbp_django.wsgi --log-file -
